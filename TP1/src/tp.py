04a7ec537ef94be3bce9fe2af34461adbc8bf436
jgarrone <javier3653@gmail.com>
2015-09-08 22:22:01 -0300

    Printeado de paquetes ARP en tiempo real y acomodado de codigo

--- TP1/src/tp.py
+++ TP1/src/tp.py
@@ -8,72 +8,57 @@ pkts = []
 
 def proccessPacket(pkt):
 	global S
-	global pkts	
+	global pkts
+
 	if hasattr(pkt, 'type'):
 		S.append(pkt.type)
 		pkts.append(pkt)
+		
+		if isARP(pkt):
+			printARPdata(pkt)
+	
 	else:
 		print 'Packet has no attribute type' # Comentar en el informe que ignoramos los paquetes sin tipo
+
+def isARP(pkt):
+	return hasattr(pkt, 'type') and pkt.type == 2054 #2054 es ARP, 2048 es IP
+
+def printARPdata(pkt):
+	ip_src, mac_src = pkt.psrc, pkt.src
+	ip_dst, mac_dst = pkt.pdst, pkt.dst
+	if pkt.op == 1:
+		print 'Who has? ip {}, tell ip {}, at mac {}'.format(ip_dst, ip_src, mac_src)
+	elif pkt.op == 2:
+		print 'The ip {} is at mac {}, telling ip {}, at mac {}'.format(ip_src, mac_src, ip_dst, mac_dst)
 
 def entropyByType():
 	global S
 		
 	type_to_count = {x: S.count(x) for x in S}
-	#print type_to_count
-	
-	type_to_prob = {}
-	for type, count in type_to_count.iteritems():
-		type_to_prob[type] = float(count)/float(len(S))
-	print "Probabilidad de la fuente S:"
-	print type_to_prob
-	print '-----------------------------'
-	
-	type_to_info = {}
-	for type, prob in type_to_prob.iteritems():
-		type_to_info[type] = -log(prob, 2)
-	#print type_to_info
-	
-	entropia = 0
-	for type, prob in type_to_prob.iteritems():
-		info = type_to_info[type]
-		info_prob_prod = prob*info
-		entropia += info_prob_prod
-	print "Entropia de la fuente S: "
-	print entropia
-	print '-----------------------------'
-	
+	S_length = float(len(S))
+	type_to_prob = {type: float(count)/S_length for type, count in type_to_count.iteritems()}
+	type_to_info = {type: -log(prob, 2) for type, prob in type_to_prob.iteritems()}
+	entropia = sum( [ type_to_info[type]*prob for type, prob in type_to_prob.iteritems() ] )
+
+	print "Probabilidad de la fuente S: {} \n -----------------------------".format(type_to_prob)
+	print "Entropia de la fuente S: {} \n -----------------------------".format(entropia)
+
 	return '---Fuente S--- OK'
 	
-
 def entropyByNodes():
 	global pkts
 	global S1
 	
-	for pkt in pkts:
-		if pkt.type == 2054: #2054 es ARP, 2048 es IP
-			S1.append(pkt)
-	
-	for pkt in S1:
-		if pkt.op in (1,2): #who-has o is-at
-			ip_src = pkt.psrc
-			mac_src = pkt.src
-			ip_dst = pkt.pdst
-			mac_dst = pkt.dst			
-			if pkt.op == 1:
-				op = 'who-has'
-				print 'Who has? ip {}, tell ip {}, at mac {}'.format(ip_dst, ip_src, mac_src)
-			else:
-				op = 'is-at'				
-				print 'The ip {} is at mac {}, telling ip {}, at mac {}'.format(ip_src, mac_src, ip_dst, mac_dst)
+	S1 = filter(lambda pkt: isARP(pkt), pkts)
 
-			print(ip_src, mac_src, ip_dst, mac_dst, op)
-			
 	return '---Fuente S1--- Ok' 
-		
+	
+
 if __name__ == '__main__':
 	print "TP1: Wiretapping"
 
-	sniff(prn=proccessPacket, store=0, timeout=5) #Cambiar el timeOut (segundos)
+	sniff(prn=proccessPacket, store=0, timeout=10) #Cambiar el timeOut (segundos)
+	
 	print entropyByType()	
 	print entropyByNodes()
 	
